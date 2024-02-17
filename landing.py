7c2425ad66af5faa92f5612849d2a278f612bfa1
Emmet Halm <emmet.halm@gmail.com>
2024-02-17 01:51:17 -0800

    Moved all functions to controller.py

--- landing.py
+++ landing.py
@@ -6,14 +6,13 @@ from olympe.messages.ardrone3.Piloting import TakeOff, Landing
 DRONE_IP = os.environ.get("DRONE_IP", "10.202.0.1")
 
 
-def test_takeoff():
+def landing():
     drone = olympe.Drone(DRONE_IP)
     drone.connect()
-    assert drone(TakeOff()).wait().success()
-    time.sleep(10)
+    
     assert drone(Landing()).wait().success()
     drone.disconnect()
 
 
 if __name__ == "__main__":
-    test_takeoff()
+    landing()
